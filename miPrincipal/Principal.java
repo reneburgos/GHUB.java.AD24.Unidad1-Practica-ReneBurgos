73a7569903d3ffb2f2b3c70eaa25e57922f0b48e
rene burgos <166579387+reneburgos@users.noreply.github.com>
2024-08-27 15:14:30 +0000

    cambios en principal

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,14 @@
 package miPrincipal;
 
-public class Principal {
-    public String getGreeting() {
+public class Principal 
+{
+    public String getGreeting() 
+    {
         return "Hola Mundo!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
        // System.out.println(new Principal().getGreeting());
 
        // PROBAR CLASE RATIONAL
