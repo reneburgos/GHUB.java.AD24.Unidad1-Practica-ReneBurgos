06ef15329da1b5a9a0040206ed46b74f63d03064
rene burgos <166579387+reneburgos@users.noreply.github.com>
2024-08-27 15:47:49 +0000

    correccion

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,12 +1,10 @@
 package miPrincipal;
-
+public class Principal
+{
     public static void main(String[] args) 
-    {
-       // System.out.println(new Principal().getGreeting());
-
-       // PROBAR CLASE RATIONAL
-       Rational r1 = new Rational(5,3);
-       Rational r2 = new Rational(4,3);
+    {   // PROBAR CLASE RATIONAL
+       Rational r1 = new Rational(a:5,b:3);
+       Rational r2 = new Rational(a:4,b:3);
        Rational r3 = new Rational();
        r3 = r1.add(r1,r2);
 
@@ -17,6 +15,6 @@ package miPrincipal;
        System.out.println("Resultado de la multiplicacion es: "+r4);
 
        rational r5 = new Rational(a:7, b:3);
-       System.out.println("Resultado de la comparacion de "+r3+" y "+r5+" es: "+);
+       System.out.println("Resultado de la comparacion de "+r3+" y "+r5+" es: ");
     }
 }
